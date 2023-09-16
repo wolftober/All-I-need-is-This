ce0d3b12dfddf80f06e2375e36d6998b9a2efcb2
bvaic <vaiciulis.ben@gmail.com>
2023-09-16 11:58:53 -0500

    I'm definitely doing something
    - added walls to my scene for testing
    - added colliders for the enemy
    - added reloading framework (not finished) for the basic gun

--- All I need is This/Assets/Scripts/Player/PistolScript.cs
+++ All I need is This/Assets/Scripts/Player/PistolScript.cs
@@ -6,18 +6,44 @@ public class PistolScript : MonoBehaviour
 {
     public Transform firepoint;
     public GameObject bulletPrefab;
+    public int magazineSize = 6;
+    public int currentAmmo = 6;
 
     void Update()
     {
         // When left mouse button is pressed
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            // will reload after player uses last boolet to keep sanity
+            if (currentAmmo > 1)
+            {
+                currentAmmo--;
+                Shoot();
+            }
+            else if (currentAmmo == 1)
+            {
+                // shoot and reload
+                currentAmmo--;
+                Shoot();
+                Reload();
+            }
+        }
+
+        // when reload button is pressed (like R)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
         }
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+
+    void Reload()
+    {
+        Debug.Log("instant reload...");
+        currentAmmo = magazineSize;
     }
 }
