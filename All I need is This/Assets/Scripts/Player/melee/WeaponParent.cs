747d495401754d76c2b7093a4f255faa72b8aa80
wolftober <alexcorbellini@gmail.com>
2023-12-06 14:35:12 -0600

    barebones new weapon system

---
+++ All I need is This/Assets/Scripts/Player/melee/WeaponParent.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParent : MonoBehaviour
+{
+    public SpriteRenderer characterRenderer, weaponRenderer;
+    public Vector2 Pointerpos { get; set; }
+
+    private void Update()
+    {
+        Vector2 direction = (Pointerpos - (Vector2)transform.position).normalized;
+        transform.right = direction;
+
+        Vector2 scale = transform.localScale;
+        if(direction.x < 0)
+        {
+            scale.y = -1;
+        }else if(direction.x > 0)
+        {
+            scale.y = 1;
+        }
+        transform.localScale = scale;
+
+        if(transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180)
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder - 1;
+        }
+        else
+        {
+            weaponRenderer.sortingOrder = characterRenderer.sortingOrder + 1;
+        }
+    }
+}
